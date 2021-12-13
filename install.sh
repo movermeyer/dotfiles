c133f1cfbfe393ffd3baea7e5744870a33aafa91
Michael Overmeyer <michael.overmeyer@shopify.com>
2021-12-13 11:06:43 -0500

    Temporarily comment out the installation

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ if [ -n $ZSH_NAME ]
 then
 
     # Install Oh My Zsh
-    if [ ! -d ~/.oh-my-zsh ]
-    then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    fi
+    # if [ ! -d ~/.oh-my-zsh ]
+    # then
+    #     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # fi
 
     # Configure Oh My Zsh
     rm -rf ~/dotfiles/.oh-my-zsh/custom
